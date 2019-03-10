46b77a8a8970f3f65ca705a5a28e920314bee420
Bruno Croci <crocidb@gmail.com>
2019-03-10 14:48:31 +0000

    added more objects to test material

--- src/tracer/mod.rs
+++ src/tracer/mod.rs
@@ -52,13 +52,15 @@ fn color(ray:&Ray, world:&impl Hittable, depth:i32) -> Vector3<f32> {
 
 pub fn trace(width:u32, height:u32) -> Vec<u32> {
     let mut pixel_data = Vec::new();
-    let ns = 5;
+    let ns = 10;
 
     let mut rng = rand::thread_rng();
 
     let mut world = HittableList::new();
-    world.add_hittable(Sphere::<lambertian::Lambertian>::new::<lambertian::Lambertian>(Vector3::new(0.0, 0.0, -1.0), 0.5, lambertian::Lambertian::new(Vector3::new(1.0, 0.0, 1.0))));
-    world.add_hittable(Sphere::<lambertian::Lambertian>::new::<lambertian::Lambertian>(Vector3::new(0.0, -100.5, -1.0), 100.0, lambertian::Lambertian::new(Vector3::new(1.0, 0.0, 1.0))));
+    world.add_hittable(Sphere::<lambertian::Lambertian>::new::<lambertian::Lambertian>(Vector3::new(0.0, 0.0, -1.0), 0.5, lambertian::Lambertian::new(Vector3::new(0.4, 0.1, 0.3))));
+    world.add_hittable(Sphere::<metal::Metal>::new::<metal::Metal>(Vector3::new(1.0, 0.0, -1.0), 0.5, metal::Metal::new(Vector3::new(0.4, 0.2, 0.6))));
+    world.add_hittable(Sphere::<metal::Metal>::new::<metal::Metal>(Vector3::new(-1.0, 0.0, -1.0), 0.5, metal::Metal::new(Vector3::new(0.8, 0.8, 0.7))));
+    world.add_hittable(Sphere::<lambertian::Lambertian>::new::<lambertian::Lambertian>(Vector3::new(0.0, -100.5, -1.0), 100.0, lambertian::Lambertian::new(Vector3::new(0.2, 0.8, 0.4))));
 
     let camera = Camera::new();
 
